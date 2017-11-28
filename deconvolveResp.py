e52bc3dd1f3815264d36eb3dff7c96407ad61f9c
Schramm <kschramm@usgs.gov>
2017-11-28 13:30:27 -0700

    replot GSN4/10 with better labels

--- deconvolveResp.py
+++ deconvolveResp.py
@@ -32,10 +32,10 @@ print(etime.julday)
 # information about the sensors. edit before running
 samprate=200.
 network = "XX"
-station = ["TST1","MOFO2"]
-channel = ["00","00"]
+station = ["TST1","GSN4"]
+channel = ["00","10"]
 component = ["EH0","EHZ"]
-sensor = ["STS-2HG","STS-6"]
+sensor = ["STS-2HG","STS-2HG"]
 
 labelRef="reference sensor: "+ sensor[0] + ' on ' + station[0]
 labelNom="test sensor: "+ sensor[1] + ' on ' + station[1]
