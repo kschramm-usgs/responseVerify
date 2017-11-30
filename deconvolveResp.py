08f53d39a1169c8a105e61c1feec753536cc3e2a
Schramm <kschramm@usgs.gov>
2017-11-30 10:08:39 -0700

    updated both t360 gsn1,2 plots

--- deconvolveResp.py
+++ deconvolveResp.py
@@ -33,10 +33,10 @@ print(etime.julday)
 # information about the sensors. edit before running
 samprate=200.
 network = "XX"
-station = ["TST1","TST6"]
+station = ["TST1","GSN2"]
 channel = ["00","00"]
-component = ["EH0","EH0"]
-sensor = ["STS-2HG","T-compact"]
+component = ["EH0","EHZ"]
+sensor = ["STS-2HG","T-360"]
 
 labelRef="reference sensor: "+ sensor[0] + ' on ' + station[0]
 labelNom="test sensor: "+ sensor[1] + ' on ' + station[1]
